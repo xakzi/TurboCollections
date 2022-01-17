a54cb038be3aecb3098ee8239e81adc6192b3a0a
Emanoel Kouriat <emanoel.kouriat@student.forsbergsskola.se>
2022-01-18 00:03:52 +0100

    zxc

---
+++ TurboCollections.Test/TurboListTests.cs
@@ -0,0 +1,97 @@
+using NUnit.Framework;
+
+namespace TurboCollections.Test
+{
+    public class TurboListTests
+    {
+        [Test]
+        public void NewListIsEmpty()
+        {
+            var list = new TurboList<int>();
+            Assert.Zero(list.Count);
+        }
+
+        [Test]
+        public void AddingAnElementIncreasesCountToOne()
+        {
+            var list = new TurboList<int>();
+            list.Add(5);
+            Assert.AreEqual(1, list.Count);
+        }
+        
+        [Test, TestCase(5), TestCase(7)]
+        public void AddingMultipleElementsIncreasesTheCount(int numberOfElements)
+        {
+            var list = new TurboList<int>();
+            for (int i = 0; i < numberOfElements; i++)
+            {
+                list.Add(5);
+            }
+            Assert.AreEqual(numberOfElements, list.Count);
+        }
+
+        [Test]
+        public void AnAddedElementCanBeGet()
+        {
+            var list = new TurboList<int>();
+            list.Add(1337);
+            Assert.AreEqual(1337, list.Get(0));
+        }
+
+        [Test]
+        public void RemoveAllElements()
+        {
+            var list = new TurboList<int>();
+            list.Clear();
+            Assert.AreEqual(0, list.Count);
+        }
+
+        [Test]
+        // removes one item from the list. If the 4th item is removed, then the 5th item becomes the 4th, the 6th becomes the 5th and so on.
+        public void RemoveOneSpecificItemAndMoveTheRestToTheLeftFromTheRightOfIt()
+        {
+            var list = new TurboList<int>(); 
+            list.Add(5);
+            list.Add(2);
+            list.Add(6);
+            list.RemoveAt(1);
+            Assert.AreEqual(2, list.Count);
+        }
+
+        [Test]
+        public void SeeIfItemExists()
+        {
+            var list = new TurboList<int>();
+            list.Add(24);
+            list.Add(4);
+            list.Add(53);
+            list.Add(10);
+            list.Add(7);
+            Assert.True(list.Contains(4));
+        }
+        
+        [Test]
+        public void SeeIfItemDoesntExist()
+        {
+            var list = new TurboList<int>();
+            list.Add(24);
+            list.Add(4);
+            list.Add(53);
+            list.Add(10);
+            list.Add(7);
+            Assert.False(list.Contains(5));
+        }
+
+        [Test]
+        public void ReturnIndexOfGivenItem()
+        {
+            var list = new TurboList<int>();
+            list.Add(24);
+            list.Add(4);
+            list.Add(53);
+            list.Add(10);
+            list.Add(7);
+            Assert.AreEqual(1,list.IndexOf(4));
+        }
+    }
+}
--- TurboCollections/TurboList.cs
+++ TurboCollections/TurboList.cs
@@ -4,9 +4,66 @@ namespace TurboCollections
 {
     public class TurboList<T>
     {
+        public int Count => items.Length;
+        private T[] items = Array.Empty<T>();
         public TurboList()
         {
             Console.WriteLine("Hello, Turbo!");
+        }
+        public void Add(T item)
+        {
+            //Resize Array, new Array, get counts, put new array to our "List"
+            T[] newArray = new T[Count + 1];
+            for (int i = 0; i < Count; i++)
+            {
+                newArray[i] = items[i];
+            }
+
+            newArray[Count] = item;
+            items = newArray;
+        }
+        public T Get(int index)
+        {
+            return items[index];
+        }
+        public void Clear()
+        {
+            items = Array.Empty<T>();
+        }
+        public void RemoveAt(int index)
+        {
+            T[] newArray = new T[Count - 1];
+            for (int i = 0; i < Count; i++){
+                if (i == index){
+                    break;
+                }
+                newArray[i] = items[i];
+            }
+            items = newArray;
+        }
+        public bool Contains(T item)
+        {
+            foreach (var elements in items)
+            {
+                if(elements.Equals(item))
+                return true;
+            }
+
+            return false;
+        }
+        public int IndexOf(T item)
+        {
+            var temp = 0;
+            foreach (var elements in items)
+            {
+                if (elements.Equals(item))
+                return temp;
+                else
+                {
+                    temp++;
+                }
+            }
+            return -1;
         }
     }
 }
