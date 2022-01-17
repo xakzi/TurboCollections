a30cbce05c3e581c97ebd9e95c055625e7420d30
Emanoel Kouriat <emanoel.kouriat@student.forsbergsskola.se>
2022-01-17 14:18:26 +0100

    Add CustomerManagement Project

---
+++ CustomerManagement/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using TurboCollections;
+
+namespace CustomerManagement 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var turboList = new TurboList<float>();
+        }
+    }
+}
--- TurboCollections/TurboList.cs
+++ TurboCollections/TurboList.cs
@@ -4,5 +4,9 @@ namespace TurboCollections
 {
     public class TurboList<T>
     {
+        public TurboList()
+        {
+            Console.WriteLine("Hello, Turbo!");
+        }
     }
 }
