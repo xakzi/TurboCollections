04229f4abba933ccad9deb97e6c129552bab7f2e
Emanoel Kouriat <emanoel.kouriat@student.forsbergsskola.se>
2022-02-04 09:54:46 +0100

    QueueList & Tests Added

---
+++ TurboCollections.Test/TurboQueueTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using NUnit.Framework;
+using TurboCollections;
+
+namespace TurboCollections.Test
+{
+    [TestFixture]
+    public class TurboQueueTests
+    {
+        [Test]
+        public void NewEmptyQueueList()
+        {
+            var newQueueList = new TurboQueue<int>();
+            Assert.AreEqual(0, newQueueList.Count);
+        }
+
+        [Test]
+        public void AddToQueueList()
+        {
+            TurboQueue<int> queueList = new TurboQueue<int>();
+            queueList.Enqueue(1);
+            queueList.Enqueue(3);
+            queueList.Enqueue(3);
+            queueList.Enqueue(7);
+            Assert.AreEqual(4, queueList.Count);
+        }
+
+        [Test]
+        public void PeekFirstItem()
+        {
+            TurboQueue<int> queueList = new TurboQueue<int>();
+            queueList.Enqueue(1);
+            queueList.Enqueue(3);
+            queueList.Enqueue(3);
+            queueList.Enqueue(7);
+            Assert.AreEqual(1, queueList.Peek());
+        }
+        [Test]
+        public void DequeueRemovesFirstItem()
+        {
+            TurboQueue<int> queueList = new TurboQueue<int>();
+            queueList.Enqueue(1);
+            queueList.Enqueue(3);
+            queueList.Enqueue(3);
+            queueList.Enqueue(7);
+            Assert.AreEqual(1, queueList.Dequeue());
+            queueList.Dequeue();
+            Assert.AreEqual(3, queueList.Dequeue());
+        }
+
+        [Test]
+        public void ClearQueueList()
+        {
+            TurboQueue<int> queueList = new TurboQueue<int>();
+            queueList.Enqueue(1);
+            queueList.Enqueue(3);
+            queueList.Enqueue(3);
+            queueList.Enqueue(7);
+            queueList.Clear();
+            Assert.AreEqual(0, queueList.Count);
+        }
+    }
+}
---
+++ TurboCollections/TurboQueue.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+
+
+namespace TurboCollections
+{
+    public class TurboQueue<T>
+    {
+        private int _indexNumber;
+        private T[] _items = new T[2];
+        public int Count { get; private set; }
+
+        public void Enqueue(T item)
+        {
+            if (Count == _items.Length)
+                Resize();
+            _items[Count] = item;
+            Count++;
+        }
+        
+        public T Peek()
+        {
+            foreach (var item in _items)
+                for (int i = _indexNumber; i < Count + _indexNumber; i++)
+                    _items[i - _indexNumber] = _items[i];
+            return _items[_indexNumber];
+        }
+
+        void Resize()
+        {
+            var result = new T[_items.Length * 2];
+            for (int i = 0; i < Count; i++)
+                result[i] = _items[i];
+            _items = result;
+        }
+
+
+        public T Dequeue()
+        {
+            T item = _items[_indexNumber];
+            _items[_indexNumber] = default(T);
+            _indexNumber++;
+            return item;
+        }
+
+        public void Clear()
+        {
+            _indexNumber = 0;
+            Count = 0;
+            _items = Array.Empty<T>();
+        }
+    }
+}
